c31d943e5f3b0186f644ba40de67f50e35d770a7
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-03 17:12:36 +0300

    Initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Waveline Media on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
